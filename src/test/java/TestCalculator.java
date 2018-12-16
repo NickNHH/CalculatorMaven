9fe848b9e86207a34f74a1a6cc5246deae20d5f9
nico <nico.tuscano@adnovum.ch>
2018-12-16 21:27:05 +0100

    Implement first JUnit test for addition.

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,9 +1,11 @@
 import static org.junit.Assert.*;
 
 public class TestCalculator {
+    private Calculator calculator;
 
     @org.junit.Test
-    public void addition() {
-
+    public void testAddition() {
+        calculator = new Calculator();
+        assertEquals(60, calculator.addition(10, 50));
     }
 }
