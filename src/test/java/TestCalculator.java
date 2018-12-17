3d47d237fcf89ec146093719ea99d71e297b901c
nico <nico.tuscano@adnovum.ch>
2018-12-17 08:18:59 +0100

    Add setup method in test class.

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,9 +1,15 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TestCalculator {
-    private Calculator calculator = new Calculator();
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
 
     @Test
     public void testAddition() {
