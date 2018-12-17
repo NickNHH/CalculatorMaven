1da5dcc0da45d3fe49523384f4fafda0b438f7f6
nico <nico.tuscano@adnovum.ch>
2018-12-17 09:03:00 +0100

    Add tests for division method.

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -17,7 +17,23 @@ public class TestCalculator {
     }
 
     @Test
-    public void subtraction() {
+    public void testSubtraction() {
         assertEquals(10, calculator.subtraction(20, 10));
+    }
+
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calculator.division(40, 20));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
+        assertEquals(0, calculator.division(10, 0));
+    }
+
+    @Test
+    public void testDivisionByZeroNotRaised() throws ArithmeticException {
+        assertEquals(0, calculator.division(10, 0));
     }
 }
