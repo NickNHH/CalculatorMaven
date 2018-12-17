d4bea6fd73ec2cdd08194769f35d6a7b327438a0
nico <nico.tuscano@adnovum.ch>
2018-12-17 08:38:47 +0100

    Implement method to divide.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,4 +7,8 @@ class Calculator {
     int subtraction(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
+
+    int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
 }
