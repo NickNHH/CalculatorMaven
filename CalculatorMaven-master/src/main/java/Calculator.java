6e4ec38b4499d73e31283bc7b942c085aabc2522
TheNiggo <nick200108@gmail.com>
2019-01-17 21:08:30 +0100

    Initial commit

---
+++ CalculatorMaven-master/src/main/java/Calculator.java
@@ -0,0 +1,18 @@
+class Calculator {
+
+    int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+
+    int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+}
---
+++ CalculatorMaven-master/src/test/java/TestCalculator.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestCalculator {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testAddition() {
+        assertEquals(60, calculator.addition(10, 50));
+    }
+
+    @Test
+    public void testSubtraction() {
+        assertEquals(10, calculator.subtraction(20, 10));
+    }
+
+    @Test
+    public void testDivision() {
+        assertEquals(2, calculator.division(40, 20));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivisionByZero() {
+        assertEquals(0, calculator.division(10, 0));
+    }
+
+    @Test
+    public void testDivisionByZeroNotRaised() throws StackOverflowError {
+        assertEquals(10, calculator.division(10, 1));
+    }
+}
